bfefa1126074e5d61dd9da3de8ec9d8d23502841
vasudev7476 <jrvickyvm@gmail.com>
2025-02-22 19:27:33 +0530

    config mismatch

--- src/main/java/com/example/yamlvalidator/config/AwsConfig.java
+++ src/main/java/com/example/yamlvalidator/config/AwsConfig.java
@@ -1,19 +1,32 @@
 package com.example.yamlvalidator.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.sns.SnsClient;
 
 @Configuration
 public class AwsConfig {
+    @Value("${aws.accessKeyId}")
+    private String accessKeyId;
+
+    @Value("${aws.secretKey}")
+    private String secretKey;
+
+    @Value("${aws.region}")
+    private String region;
+    
     @Bean
     public SnsClient snsClient() {
-        AwsCredentials credentials = DefaultCredentialsProvider.create().resolveCredentials();
+          AwsBasicCredentials awsCreds = AwsBasicCredentials.create(accessKeyId, secretKey);
         return SnsClient.builder()
                 .region(Region.US_EAST_1)  // Set your AWS region
-                .credentialsProvider(DefaultCredentialsProvider.create())
+                .credentialsProvider(StaticCredentialsProvider.create(awsCreds))
                 .build();
     }
 }
