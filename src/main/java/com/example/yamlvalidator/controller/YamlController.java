b5abc62b6752de39bbbc5fad59a6d32c56d097be
Vasudev7476 <jrvickyvm@gmail.com>
2025-02-23 13:59:52 +0530

    Update YamlController.java

--- src/main/java/com/example/yamlvalidator/controller/YamlController.java
+++ src/main/java/com/example/yamlvalidator/controller/YamlController.java
@@ -12,6 +12,8 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 
+// YAML controller
+
 @RestController
 @RequestMapping("/api/yaml")
 public class YamlController {
